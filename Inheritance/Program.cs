2c5e134e168ea72a2d1c4f50f5344b3dd37392cd
Ashish Shrestha <ashish.shrestha@truenary.com>
2025-05-29 13:18:24 +0545

    feat: inheritance with animal class

---
+++ Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿// Base class
+public class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Eating food...");
+    }
+}
+
+// Derived class Dog
+public class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Barking...");
+    }
+}
+
+// Derived class Cat
+public class Cat : Animal
+{
+    public void Meow()
+    {
+        Console.WriteLine("Meowing...");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        dog.Eat();
+        dog.Bark();
+
+        Cat cat = new Cat();
+        cat.Eat();
+        cat.Meow();
+    }
+}
