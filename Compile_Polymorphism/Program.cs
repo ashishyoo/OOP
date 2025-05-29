2fbf30f5956d27c6e4977380a8db57f48a59756d
Ashish Shrestha <ashish.shrestha@truenary.com>
2025-05-29 13:23:22 +0545

    feat: compile polymorphism with method overloading

---
+++ Compile_Polymorphism/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Calculator
+{
+    // Add two integers
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    // Add three integers
+    public int Add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    // Add two doubles
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Calculator calc = new Calculator();
+
+        Console.WriteLine(calc.Add(2, 3));
+        Console.WriteLine(calc.Add(2, 3, 4));
+        Console.WriteLine(calc.Add(2.5, 3.2));
+    }
+}
