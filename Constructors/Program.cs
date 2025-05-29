8e8ee7800342d6be84f242195587a017a0015df0
Ashish Shrestha <ashish.shrestha@truenary.com>
2025-05-29 12:48:32 +0545

    feat: create constructors

---
+++ Constructors/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Person
+{
+    private int id;
+    private string name;
+
+    public Person() // default constructor
+    {
+        id = 0;
+        name = "undefined";
+        Console.WriteLine("Person object created..");
+    }
+
+    public Person(int id, string name) // parameterized constructor
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void Introduce()
+    {
+        Console.WriteLine($"Hi, my name is {name} and my id is {id}");
+    }
+}
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Person person1 = new Person(); // calls Person()
+        person1.Introduce();
+        Person person2 = new Person(1, "Yeju"); // calls Person(int id, string name)
+        person2.Introduce();
+    }
+}
