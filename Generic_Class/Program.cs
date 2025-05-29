084439b4ee5693fea5af1060a6b129eb4676f2ad
Ashish Shrestha <ashish.shrestha@truenary.com>
2025-05-29 14:11:46 +0545

    feat: implement generic class

---
+++ Generic_Class/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Box<T>
+{
+    private T Value;
+
+    public Box(T value)
+    {
+        Value = value;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Box contains: " + Value);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Box<int> intBox = new Box<int>(100);
+        intBox.Display();  // Output: Box contains: 100
+
+        Box<string> strBox = new Box<string>("Apple");
+        strBox.Display();  // Output: Box contains: Apple
+    }
+}
