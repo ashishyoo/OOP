1e0f800a14782c42ae5496a3544e089c4b9df166
Ashish Shrestha <ashish.shrestha@truenary.com>
2025-05-29 12:25:53 +0545

    feat: create object

---
+++ Objects/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Person
+{
+    public string name;
+
+    public void Introduce()
+    {
+        Console.WriteLine($"Hi, my name is {name}");
+    }
+}
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Person person = new Person(); // creating Person object
+        person.name = "Yeju";
+        person.Introduce();
+    }
+}
